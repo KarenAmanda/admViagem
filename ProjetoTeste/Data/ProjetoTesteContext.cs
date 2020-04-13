2775b46996d0077b2642e0bbd3c1931380115413
Karen Corrêa <karenamanda_81@hotmail.com>
2020-04-13 12:37:23 -0300

    Configuração do mysql e atualização para o dot net core 3.1

---
+++ ProjetoTeste/Data/ProjetoTesteContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoTeste.Models;
+
+namespace ProjetoTeste.Data
+{
+    public class ProjetoTesteContext : DbContext
+    {
+        public ProjetoTesteContext (DbContextOptions<ProjetoTesteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoTeste.Models.UserModel> UserModel { get; set; }
+    }
+}
--- ProjetoTeste/Startup.cs
+++ ProjetoTeste/Startup.cs
@@ -31,8 +31,8 @@ namespace ProjetoTeste
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +52,8 @@ namespace ProjetoTeste
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+
+
 
             app.UseMvc(routes =>
             {
