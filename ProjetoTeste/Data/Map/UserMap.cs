c12232aba17773ef6bdb8e222761a5a7ef30d7a2
Karen Corrêa <karenamanda_81@hotmail.com>
2020-04-15 10:36:16 -0300

    Conexão com BD.

---
+++ ProjetoTeste/Data/Map/UserMap.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProjetoTeste.Models;
+
+namespace ProjetoTeste.Data.Map
+{
+    public class UserMap : IEntityTypeConfiguration<UserModel>
+    {
+        public void Configure(EntityTypeBuilder<UserModel> builder)
+        {
+            builder.ToTable("Usuario");
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Name).IsRequired().HasColumnName("Nome");
+            builder.Property(c => c.Birthday).IsRequired().HasColumnName("DataNascimento");
+            builder.Property(c => c.Email).IsRequired().HasColumnName("Email");
+            builder.Property(c => c.Telephone).IsRequired().HasColumnName("Telefone");
+        }
+    }
+}
